b4237fbc08a1d4e183f1108c0f335e38a79be020
Luke <luke.freeman4@gmail.com>
2022-12-09 14:01:12 -0600

    I've created the database for Inventory. Setting up the model

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 # import models
+from models import (Base, session, Item, engine)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+
+
 # main menu - add, search, analysis, exit and view
 # functions add item to database
 # edit items
--- models.py
+++ models.py
@@ -1,3 +1,27 @@
+from sqlalchemy import (create_engine, Column, Integer, String, DATE)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///Inventory.db', echo = False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key = True)
+    name = Column('Name', String)
+    date_purchased = Column('Date Purchased', DATE)
+    date_expires = Column('Date Expires', DATE)
+    item_quantity = Column('Quantity', Integer)
+    item_price = Column('Price', Integer)
+    item_location = Column('Location', String)
+
+    def __repr__(self):
+        return f'Name: {self.name} Date Purchased: {self.date_purchased} Date Expires: {self.date_expires} Quantity: {self.item_quantity} Price: {self.item_price} Location: {self.item_location}'
+
 # create a database
 # inventory.db, locations.db, 
 # create a model 1
