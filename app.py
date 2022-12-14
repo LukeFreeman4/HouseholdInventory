55df8f12c5cc118e445dbe6111988a599fa2234f
Luke <luke.freeman4@gmail.com>
2022-12-14 05:59:51 -0600

    I was able to create the option to exit my app loop and to view the database. Made notes on possible way forward for adding items

--- app.py
+++ app.py
@@ -1,14 +1,66 @@
-# import models
+# To activate virtual enviornment use 'source env/bin/activate'
+
 from models import (Base, session, Item, engine)
+import pandas as pd
+
+def menu():
+    while True:
+        print('''
+                \rINVENTORTY
+                \r1.) Add Item
+                \r2.) View All Items
+                \r3.) Search for Item
+                \r4.) exit''')
+        user_choice = int(input('What would you like to do? '))
+        return user_choice
+
+def view_inventory():
+    user_table = pd.read_sql_table(table_name = "items", con=engine)
+    print(user_table)
+
+def add_new():
+    pass
+    # insert data into columns. 
+        # Item.inster().values(
+            # Id = 'number', --should autoincrement
+            # Name = 'item name') -- but assing these to variatbles and ask for user intput. 
+    # ask for input on each column
+    # should give option to add another of greak so needs to be a loop
+
+
+def run_app():
+    app_running = True
+    while app_running:
+        menu_choice = menu()
+        if menu_choice == 1:
+            view_inventory()
+            input('press enter to continue')
+            # View Inventory
+        elif menu_choice == 2:
+            pass
+            # add new
+        elif menu_choice == 3:
+            pass
+            # data analysis
+        elif menu_choice == 4:
+            app_running = False
+            break
+        #limit user choice to the inputs listed for options. 
+        
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    run_app()
 
 
-# main menu - add, search, analysis, exit and view
-# functions add item to database
-# edit items
-# delete items
-# search function
-# data cleaning
-# loop that runs the program
+
+
+               
+
+
+
+# edit items function
+# delete items function
+# search function function
+# data cleaning -- may not need
+# loop that runs the program -- the runn_app should be what I want
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import (create_engine, Column, Integer, String, DATE)
+from sqlalchemy import (create_engine, Column, Integer, String, DATE, MetaData, DATETIME)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pandas as pd
+from datetime import datetime
 
 
 engine = create_engine('sqlite:///Inventory.db', echo = False)
@@ -12,15 +14,17 @@ class Item(Base):
     __tablename__ = 'items'
 
     id = Column(Integer, primary_key = True)
-    name = Column('Name', String)
-    date_purchased = Column('Date Purchased', DATE)
+    name = Column('Name', String, nullable = False)
+    date_purchased = Column('Date Purchased', DATE, nullable = False)
     date_expires = Column('Date Expires', DATE)
-    item_quantity = Column('Quantity', Integer)
-    item_price = Column('Price', Integer)
-    item_location = Column('Location', String)
+    item_quantity = Column('Quantity', Integer, nullable = False)
+    item_price = Column('Price', Integer, nullable = False)
+    item_location = Column('Location', String, nullable = False)
+    
 
     def __repr__(self):
         return f'Name: {self.name} Date Purchased: {self.date_purchased} Date Expires: {self.date_expires} Quantity: {self.item_quantity} Price: {self.item_price} Location: {self.item_location}'
+
 
 # create a database
 # inventory.db, locations.db, 
